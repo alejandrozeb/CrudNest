768ae7d8b5dc3b0b0f8ee0634f5a8b5cd69a7dd4
alejandro zeballos aguilar <alejandrojzeballos@gmail.com>
2022-03-23 20:13:58 -0400

    {add} instalando next e iniciando proyecto

---
+++ apunte.js
@@ -0,0 +1,22 @@
+/* eslint-disable prettier/prettier */
+/* controller -> service-> data access
+                Datamodel 
+----comandos-------------               
+nest --version
+nest --help
+extencios vs----
+eslinq
+prettier 
+editorconfig
+----instalcion----
+npm i -g @nestjs/cli
+npm --version
+npm --help
+
+---crear proyecto-----
+nest new <<name>>
+cd <<name>>
+npm run start
+
+
+*/
